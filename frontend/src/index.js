ce14d3951e407d2bbe3f5c350845ed3334af8205
Gabril Colman <gabriecolman7@gmail.com>
2024-09-22 13:37:38 -0400

    Alteração Utf-8

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 ReactDOM.render(
-<Provider store={store}>
- <App />
-</Provider>,
-document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 );
