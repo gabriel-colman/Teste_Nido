17f7156c9886d84dcfc03fe71afe8504656fc48e
Gabril Colman <gabriecolman7@gmail.com>
2024-09-22 13:52:57 -0400

    Mudança

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home'; 
-import Login from './pages/Login'; 
-import Favorites from './pages/Favorites'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Favorites from './pages/Favorites';
 import History from './pages/History';
 
-const App = () => { 
-    return (<Router> 
-        <Switch> 
-            <Route exact path='/' component={Home} /> 
-            <Route path='/login' component={Login} /> 
-            <Route path='/favorites' component={Favorites} /> 
-            <Route path='/history' component={History} />
-          </Switch> 
-        </Router>); }
+const App = () => {
+  return (<Router>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/favorites' element={<Favorites />} />
+      <Route path='/history' element={<History />} />
+    </Routes>
+  </Router>
+  );
+}
 
 export default App;
